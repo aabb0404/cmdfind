b703f057ad00c133f767890395ea8079955d813a
johnny <dog01050435@gmail.com>
2019-08-28 12:46:45 +0800

    clean code

--- cmd/finddata.js
+++ cmd/finddata.js
@@ -1,10 +1,11 @@
-import {update} from './util/update'
 import fs from 'fs';
+import {update} from './util/update'
+
 const filename = 'test.txt';
 const encode = 'utf8';
 let networks = [];
 
-fs.readFile(filename, encode,async function(err, file) {
+fs.readFile(filename, encode, async (err, file) => {
     let message = file.split('\n');
     for (let i = 1; i < message.length - 1; i++) {
         let network = {};
@@ -16,27 +17,24 @@ fs.readFile(filename, encode,async function(err, file) {
                 networks.push(network);
             }
         }
-    for(let i = 0;i<networks.length-1;i++){
-        console.log(networks[i].deviceIP)
-        console.log(networks[i].deviceMac)
-        //await update(networks[i].deviceIP,networks.deviceMac);
-    }
+        for(let i = 0; i < networks.length - 1; i++){
+            const deviceIP = networks[i].deviceIP;
+            const deviceMac = networks[i].deviceMac;
+            await update(deviceMac, deviceIP);
+        }
 
     }
     console.log(networks);
 });
 
-const type =(message)=>{
-    if(message == '動態'){
-        return true;
-    }
-    return false
+const type = (message) => {
+    const isDynamicIp = message === '動態' ? true : false;
+    return isDynamicIp;
 }
 
-const isIngics = (message) =>{
-    if(message[0] == 'b0' && message[1]=='38')
-    {
-        return true
+const isIngics = (message) => {
+    if(message[0] === 'b0' && message[1] === '38') {
+        return true;
     }
-    else return false
+    return false;
 }
